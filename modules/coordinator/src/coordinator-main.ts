46ce84979991ff57d5928130cfbb255eb31af885
Péter Kovács <peter.kovacs0519@gmail.com>
2023-08-07 19:35:25 +0200

    LFLT-574 Implement application skeleton for Domino Coordinator  - Added CircleCI monorepo build configuration for Coordinator and Docker Agent  - Added skeletons for Coordinator and Docker-Agent

---
+++ modules/coordinator/src/coordinator-main.ts
@@ -0,0 +1,5 @@
+import DummyClass from "@coordinator/app";
+import { version } from "@coordinator-package";
+
+console.log(version);
+new DummyClass().test();
---
+++ modules/docker-agent/src/docker-agent-main.ts
@@ -0,0 +1,5 @@
+import DummyClass from "@docker-agent/app";
+import { version } from "@docker-agent-package";
+
+console.log(version);
+new DummyClass().test();
